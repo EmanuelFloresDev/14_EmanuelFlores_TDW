663729af0f3be9de89f14badce2dce959d470ebd
emanuelFlores07 <emanuel.flores@vallegrande.edu.pe>
2026-09-02 20:43:27 -0500

    Agrega proyecto slider con Flask y Tailwind

---
+++ slider-flask/app/routes/__init__.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+main = Blueprint("main", __name__)
+
+
+@main.route("/")
+def inicio():
+    return render_template("index.html")
---
+++ slider-flask/app/static/js/slider.js
@@ -0,0 +1,31 @@
+let slideActual = 0;
+
+const slides = document.querySelectorAll(".slide");
+
+
+function mostrarSlide(numero) {
+
+    if (numero >= slides.length) {
+        slideActual = 0;
+    }
+
+    if (numero < 0) {
+        slideActual = slides.length - 1;
+    }
+
+    slides.forEach(function(slide) {
+        slide.classList.remove("active");
+    });
+
+    slides[slideActual].classList.add("active");
+}
+
+
+function cambiarSlide(direccion) {
+    slideActual = slideActual + direccion;
+
+    mostrarSlide(slideActual);
+}
+
+
+mostrarSlide(slideActual);
